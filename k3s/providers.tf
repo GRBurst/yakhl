6c74510aade2e08fcf0b7a78510da5976da57f21
GRBurst <GRBurst@protonmail.com>
2024-03-28 12:08:30 +0100

    Minor provider refactoring

--- k3s/providers.tf
+++ k3s/providers.tf
@@ -1,23 +1,32 @@
+locals {
+  kube_config = "/etc/rancher/k3s/k3s.yaml" # "~/.kube/config"
+}
+
 terraform {
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "2.27.0"
+      version = "~> 2.27.0"
     }
     keycloak = {
       source = "mrparkers/keycloak"
       version = ">= 4.0.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.1"
+    }
   }
 }
 
 provider "kubernetes" {
-  config_path    = "/etc/rancher/k3s/k3s.yaml"
+  config_path    = pathexpand(local.kube_config)
   config_context = "default"
 }
 
 provider "helm" {
   kubernetes {
     config_context = "default"
+    config_path = pathexpand(local.kube_config)
   }
 }
